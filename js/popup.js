f0b36c9fb6af16bb21d33f4e03bfeb4f95d61cdc
dodal81 <djd8320@naver.com>
2021-10-14 17:36:35 +0900

    팝업 작업중

--- js/popup.js
+++ js/popup.js
@@ -1,41 +1,35 @@
 // popup_box -----------------------------------------
-var isCookie = document.cookie.indexOf("popup=done");
-console.log(isCookie);
-const popup = document.querySelector("#popup");
-console.log(popup);
+var isCookie = document.cookie.indexOf("popup=done"); 
+console.log(isCookie); 
 
 
-// if(isCookie == -1 ){
-//     popup.show(); 
+if(isCookie == -1){
+   $("#popup").show(); 
+   console.log("쿠키없음"); 
+}else{
    
-// }else{
-    
-//     popup.hide();
-    
-// }
+   $("#popup").hide();
+   console.log("쿠키있음");  
+}
 
 $("#popup .close").on("click", function(e){
-    e.preventDefault();
+   e.preventDefault(); 
+  
+   var isChecked = $("#popup").find("input[type=checkbox]").is(":checked"); 
 
-   
-    var isChecked = $("#popup").find("input[type=checkbox]").is(":checked");
-
-   
-    if(isChecked) setCookie();
-
-    $("#popup").hide();
-});
-
-
-function setCookie(time) {
-
-    var today = new Date(); 
-    var date = today.getDate();  
-
-    today.setDate(date + time); 
-
-    var duedate = today.toGMTString(); 
-
+   if(isChecked) setCookie(1); 
     
-    document.cookie = "popup=done; expires="+duedate;
-};
+   $("#popup").hide(); 
+}); 
+
+
+function setCookie(time){
+   var today = new Date(); 
+   var date = today.getDate();  
+   
+   today.setDate(date + time);  
+    
+   var duedate = today.toGMTString(); 
+
+   document.cookie = "popup=done; expires="+duedate; 
+}
