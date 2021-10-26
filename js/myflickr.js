c35b40bc8a95c89cfcf5d84fdbb9d026a25eeaae
dodal81 <djd8320@naver.com>
2021-10-27 02:25:45 +0900

    작업중

--- js/myflickr.js
+++ js/myflickr.js
@@ -1,92 +1,112 @@
 $.ajax({
-    url: "https://www.flickr.com/services/rest/?method=flickr.interestingness.getList",
-    dataType: "json",
-    data: {
-        api_key: "585462c89320318d78d565f713b9b44e",
-        per_page: 100,
-        format: "json",
-        nojsoncallback: 1,
-        privacy_filter: 1,
+    url:"https://www.flickr.com/services/rest/?method=flickr.photos.search",
+    //url:"https://www.flickr.com/services/rest/?method=flickr.interestingness.getList", 
+    dataType:"json", 
+    data:{
+        api_key:"585462c89320318d78d565f713b9b44e", 
+        per_page:500, 
+        format:"json",
+        nojsoncallback:1, 
+        privacy_filter : 1, 
+        tags :"landscape" 
     }
 })
-    .success(function (data) {
+.success(function(data){    
+    
+    let items = data.photos.photo; 
+    console.log(items);
+   
+    $("#gallery_page").append("<ul>");
+   
+    $(items).each(function(index, data){
+        let text = data.title;        
+        if(!data.title){          
+            text = "No description in this photo"; 
+        }
 
-        let items = data.photos.photo;
-
-        $("#gallery").append("<ul>");
-
-        $(items).each(function (index, data) {
-
-            let text = data.title;
-            if (!data.title) {
-                text = "No"
-            }
-
-            $("#gallery ul")
-                .append(
-                    $("<li>")
-                        .append(
-                            $("<div>").append(
-                                $("<a>").attr({
-                                    href: "https://live.staticflickr.com/" + data.server + "/" + data.id + "_" + data.secret + "_b.jpg"
+        $("#gallery_page ul")
+            .append(
+                $("<li>")   
+                    .append(
+                        $("<div>").append(
+                            $("<a>").attr({
+                                href : "https://live.staticflickr.com/"+data.server+"/"+data.id+"_"+data.secret+"_b.jpg"
+                            })
+                            .append(                            
+                                $("<img class='thumb'>").attr({
+                                    src : "https://live.staticflickr.com/"+data.server+"/"+data.id+"_"+data.secret+"_m.jpg"
                                 })
-                                    .append(
-                                        $("<img>").attr({
-                                            src: "https://live.staticflickr.com/" + data.server + "/" + data.id + "_" + data.secret + "_m.jpg"
-                                        })
-                                    ),
-                                $("<p>").text(text)
-                            )
+                            ),
+                            $("<p>").text(text),    
+                                                   
+                            $("<div class='profile'>")
+                            .append(
+                                $("<img class='buddy'>").attr({
+                                    //src : "https://www.flickr.com/buddyicons/"+data.owner+".jpg"
+                                    src: "http://farm"+data.farm+".staticflickr.com/"+data.server+"/buddyicons/"+data.owner+".jpg"
+                                }),
+                                $("<span>").text(data.owner)
+                            )   
+                                                     
                         )
-                )
+                    ) 
+            )//gallery append ends       
+    });  
+    
+    //img 태그요소가 완료된 이후 해당 img요소의 전체 갯수 구함
+    const total = $("#gallery_page ul li").length;
 
-        });
+    
+    let imgNum=0;
 
+    $("#gallery_page img").each(function(index, data){   
+        data.onerror = function(){
+            $(data).attr("src", "img/default.jpg");
+        }
+        
+        data.onload = function(){            
+            imgNum++;
+            console.log(imgNum);
+           
+            //카운트되는 이미지 갯수와 (전체이미지갯수*2)값이 동일하면
+            if(imgNum === total*2){   
+                //로딩바이미지에 off를 붙여서 안보이게 처리
+                $(".loading").addClass("off");
 
-        const total = $("#gallery ul li").length;
-
-        let imgNum=0;
-
-        $("#gallery img").each(function(index, data){
-            data.onerror = function() {
-                $(data).attr("src", "img/error01.jpg");
+                new Isotope("#gallery_page ul",{
+                    itemSelector : "#gallery_page ul li",
+                    columnWidth: "#galley_page ul li",                  
+                    transitionDuration: "0.5s"
+                });   
+                
+                //gallery ul에 on을 붙여서 서서히 보이게 처리
+                $("#gallery_page ul").addClass("on");
             }
+        }        
+    }); 
+    
+    
+    
+})
+.error(function(err){
+    console.err("데이터를 호출하는데 실패했습니다"); 
+});
 
-            data.onload = function(){
-                imgNum++;
-                console.log(imgNum);
 
-                if(imgNum === total) {
+$("body").on("click", "#gallery ul li", function(e){
+    e.preventDefault(); 
 
-                    $(".loading").addClass("off");
-
-                    new Isotope("#gallery ul", {
-                        itemSelector : "#gallery ul li",
-                        columnWidth : "#gallery ul li",
-                        transitionDuration : "0.5s"
-                    });
-
-                    $("#gallery ul").addClass(".on");
-                }
-            }
-        });
-    })
-    .error(function (err) {
-        console.err("데이터를 호출하는데 실패했습니다.");
-    })
-$("body").on("click", "#gallery ul li", function (e) {
-    e.preventDefault();
-
-    let imgSrc = $(this).children("a").attr("href");
+    let imgSrc = $(this).children("a").attr("href"); 
 
     $("body").append(
-        $("<div class = 'pop'>")
+        $("<div class='pop'>")
             .append(
-                $("<img>").attr({ src: imgSrc }),
+                $("<img>").attr({ src : imgSrc}),
                 $("<span>").text("close")
             )
     )
 });
-$("body").on("click", ".pop span", function () {
-    $(".pop").remove();
+
+$("body").on("click", ".pop span", function(){
+    $(".pop").remove(); 
 });
