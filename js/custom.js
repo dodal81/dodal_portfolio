7ff41c20199769bcd208aa8b02f392d16750421e
dodal81 <djd8320@naver.com>
2021-11-08 17:49:52 +0900

    메인페이지 수정중

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,4 @@
 var btnCall = document.querySelector(".btnCall");
-
 var menuMo = document.querySelector(".menuMo");
 
 btnCall.onclick = function(e){
@@ -8,5 +7,21 @@ btnCall.onclick = function(e){
     btnCall.classList.toggle("on");
     menuMo.classList.toggle("on");
 }
+$("#gnb>li").on("mouseenter", function(){
+    $(this).find(".sub").show();
+});
+$("#gnb>li").on("mouseleave", function(){
+    $(this).find(".sub").hide();
+});
+$("#gnb>li").each(function(index){
+    $("#gnb>li").eq(index).find("a").on("focusin", function(){
+        $("#gnb>li").eq(index).find(".sub").show();
+    });
+    $("#gnb>li").eq(index).find("a").last().on("focusout", function(){
+        $("#gnb>li").eq(index).find(".sub").hide();
+    })
+});
+
+
 
 
