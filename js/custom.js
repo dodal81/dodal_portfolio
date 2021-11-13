ef87d8a0e9a966fdca9437bace46c9abb2b0b692
dodal81 <djd8320@naver.com>
2021-11-14 05:04:49 +0900

    마우스 커서 수정

--- js/custom.js
+++ js/custom.js
@@ -1,26 +1,41 @@
 var btnCall = document.querySelector(".btnCall");
 var menuMo = document.querySelector(".menuMo");
 
-btnCall.onclick = function(e){
-    e.preventDefault(); 
-    
+
+btnCall.onclick = function (e) {
+    e.preventDefault();
+
     btnCall.classList.toggle("on");
     menuMo.classList.toggle("on");
 }
-$("#gnb>li").on("mouseenter", function(){
+$("#gnb>li").on("mouseenter", function () {
     $(this).find(".sub").show();
+
 });
-$("#gnb>li").on("mouseleave", function(){
+$("#gnb>li").on("mouseleave", function () {
     $(this).find(".sub").hide();
 });
-$("#gnb>li").each(function(index){
-    $("#gnb>li").eq(index).find("a").on("focusin", function(){
+$("#gnb>li").each(function (index) {
+    $("#gnb>li").eq(index).find("a").on("focusin", function () {
         $("#gnb>li").eq(index).find(".sub").show();
     });
-    $("#gnb>li").eq(index).find("a").last().on("focusout", function(){
+    $("#gnb>li").eq(index).find("a").last().on("focusout", function () {
         $("#gnb>li").eq(index).find(".sub").hide();
     })
 });
+
+// const result = document.querySelector(".result");
+
+// document.addEventListener("mousemove", (e) => {
+//     const mouseX = e.clientX;
+//     const mouseY = e.clientY;
+//     result.style.left = mouseX + 'px';
+//     result.style.top = mouseY + 'px';
+
+//     console.log(e.clientX);
+//     console.log(e.clientY);
+// });
+
 
 
 
