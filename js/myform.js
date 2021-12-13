8be48f544e886ad356ebcefbf0631aa35714e6a4
dodal81 <djd8320@naver.com>
2021-12-14 04:22:47 +0900

    폼수정 / form vaildation작업

--- js/myform.js
+++ js/myform.js
@@ -1,113 +1,103 @@
-// const { init } = require("browser-sync");
+// 폼 입력 
+class MyForm{
 
-$(document).ready(function () {
-    setDateBox();
-});
-
-function setDateBox() {
-    const date = new Date();
-    const year = "";
-    const com_year = date.getFullYear();
-
-    $("#year").append("<option value=''>년도</option>");
-
-    for (let i = (com_year - 50); i <= (com_year + 1); i++) {
-        $("#year").append("<option value='" + i + "'>" + i + "년" + "</option>");
-    }
-
-    let month;
-    $("#month").append("<option value=''>월</option>");
-    for (let i = 1; i <= 12; i++) {
-        $("#month").append("<option value='" + i +"'>" + i + "월" + "</option>");
+    constructor(selector, options){
+ 
+       if(!selector){
+          console.error("form 선택자는 필수 입력사항입니다"); 
+          return;
+       }
+       this.init(selector); 
+       this.bindingEvent(options); 
     }
     
-    let day;
-    $("#day").append("<option value =''>일</");
-    for(let i = 1; i <= 31; i++) {
-        $("#day").append("<option value ='" + i + "'>" + i + "일" + "</option>");
-    }
-}
-
-class Myform{
-    constructor(selector, options){
-        if(!selector) {
-            console.error("form 선택자는 필수 입력사항입니다")
-            return;
-        }
-        this.Infinity(selector);
-        this.bindingEvent(options);
-    }
     init(selector){
-        this.form = $(selector);
-        this.btnsubmit = this.form.find("input[type='submit']");
+       this.form = $(selector); 
+       this.btnSubmit = this.form.find("input[type='submit']"); 
     }
+    
     bindingEvent(options){
-        options.forEach((opt)=>{
-            this.btnsubmit.on("click", (e)=>{
-                if(opt.type==="text"){
-                    if(!this.isTxt(opt.name, opt.len)) e.preventDefault();
-                }
-                if(opt.type==="email"){
-                    if(!this.isEmail(opt.name)) e.preventDefault();
-                }
-                if(opt.type==="check"){
-                    if(!this.isCheck(opt.name)) e.preventDefault();
-                }
-                // if(opt.type==="select"){
-                //     if(!this.isSelect(opt.name)) e.preventDefault();
-                // }
-                if(opt.type==="password"){
-                    if(!this.isPwd(opt.name[0], opt.name[1], opt.len)) e.preventDefault();
-                }
-            });
-        });
+      
+       options.forEach( (opt)=>{
+          this.btnSubmit.on("click", (e)=>{
+             if(opt.type==="text"){
+                if(!this.isTxt(opt.name, opt.len)) e.preventDefault(); 
+             }
+             if(opt.type==="email"){
+                if(!this.isEmail(opt.name)) e.preventDefault();  
+             }
+             if(opt.type==="password"){
+                if(!this.isPwd(opt.name[0], opt.name[1], opt.len)) e.preventDefault(); 
+             }          
+          }); 
+       }); 
+    
+       
     }
-
 
     isTxt(name, len){
-        if(len === undefined) len = 5;
+       
+       if(len === undefined ) len = 5; 
+       
+       let txt = $("[name = "+name+"]").val(); 
 
-        let txt = $("[name = "+name+"]").val();
+       if(txt.length >= len){
+          $("[name = "+name+"]").parent().find("p").remove(); 
+          return true; 
+       }else{
 
-        if(txt.length >= len){
-            $("[name = "+name+"]").parent().find("p").remove();
-            return true;
-        }else {
-            $("[name = "+name+"]").parent().find("p").remove();
-            $("[name = "+name+"]").parent().append(
-                "<p>아이디를 "+len+"자 이상 입력하세요</p>"
-            )
-            return false;
-        }
+       $("[name = "+name+"]").parent().find("p").remove(); 
+       $("[name = "+name+"]").parent().append(
+             "<p>아이디를 "+len+"자 이상 입력하세요</p>"
+          )
+          return false; 
+       }
+       
     }
+    isPhone(name, len){
+        if(len === undefined) len = 11;
+
+        let phone = 
+    }
+
     isEmail(name){
-        let txt = $("[name = "+name+"]").val();
+    
+       let txt = $("[name="+name+"]").val(); 
+    
+       //txt에 @가 포함되어 있다면 
+       if(/@/.test(txt)){
+          $("[name="+name+"]").parent().find("p").remove(); 
+          return true; 
+       }else{
 
-        if(/@/.test(txt)){
-            $("[name = "+name+"]").parent().find("p").remove();
-            return true;
-        }else{
-            $("[name = "+name+"]").parent().find("p").remove();
-            $("[name = "+name+"]").parent().append(
-                "<p>@를 포함한 전체 메일 주소를 입력하세요.</p>"
-            )
-            return false;
-        }
+          $("[name="+name+"]").parent().find("p").remove(); 
+          $("[name="+name+"]").parent().append(
+             "<p>@를 포함한 전체 메일 주소를 입력하세요.</p>"
+          )
+          return false; 
+       }
     }
 
-    isCheck(name){
-        let isCheck = $("input[name = "+name+"]").is(":checked");
-
-        if(isCheck){
-            $("input[name = "+name+"]").parent().find("p").remove();
-            return true;
-        }else{
-            $("input[name = "+name+"]").parent().find("p").remove();
-            $("input[name = "+name+"]").parent().append(
-                "<p>필수 입력사항을 체크해 주세요</p>"
-            )
-            return false;
-        }
-    }
+     
+    isPwd(name1, name2, len){
  
-}
+       let pwd1 = $("input[name="+name1+"]").val(); 
+       let pwd2 = $("input[name="+name2+"]").val(); 
+    
+       let num = /[0-9]/; 
+       let eng = /[a-zA-Z]/; 
+       let spc = /[~!@#$%^&*()_+-\[\]]/; 
+    
+       if(pwd1 === pwd2 && pwd1.length >= len && num.test(pwd1) && eng.test(pwd1) && spc.test(pwd1)){
+          $("input[name="+name1+"]").parent().find("p").remove(); 
+          return true; 
+       }else{
+          $("input[name="+name1+"]").parent().find("p").remove(); 
+          $("input[name="+name1+"]").parent().append(
+             "<p>비밀번호는 "+len+"자 이상 영문자, 숫자, 특수문자를 포함해서 동일하게 써 주세요</p>"
+          )
+          return false; 
+       }
+    }
+    
+ }
